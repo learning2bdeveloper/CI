0915324858d49f60c8696ddcbc41284a46da50f5
learning2bdeveloper <learn.2.be.a.developer@gmail.com>
2024-03-06 12:53:04 +0800

    added data table and defer scripts

--- application/helpers/template/organization_helper.php
+++ application/helpers/template/organization_helper.php
@@ -18,8 +18,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="<?=base_url('assets/css/table.css')?>">
 
+    <!-- Link DataTables CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.1/css/dataTables.bootstrap5.css">
 
+  
+<script defer src="<?=base_url('/assets/javascript/index.js')?>" ></script>
+<script defer src="<?=base_url('/assets/javascript/changeLocation.js')?>" ></script>
 
+    
 </head>
 
 <?php
@@ -29,9 +36,14 @@ function organization_footer() {
 
 ?>
     </body>
+<!-- Link DataTables JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<script src="<?=base_url('/assets/javascript/index.js')?>"></script>
-<script src="<?=base_url('/assets/javascript/changeLocation.js')?>"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.0.1/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.0.1/js/dataTables.bootstrap5.js"></script>
+
+    
 </html>
 
 <?php
